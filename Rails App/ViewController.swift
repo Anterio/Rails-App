4e743291e3c729c3ab536c7bf7ea9c0d0a7a5a07
Anterio <Arthur@Arthurs-MacBook-Pro-2.local>
2014-09-06 14:08:25 -0400

    sdfga

--- Rails App/ViewController.swift
+++ Rails App/ViewController.swift
@@ -7,9 +7,112 @@
 //
 
 import UIKit
+import CoreData
+let API = "http://ancient-taiga-9634.herokuapp.com/"
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var contentField: UITextField!
+    @IBOutlet weak var titleField: UITextField!
+    
+    @IBAction func postNew(sender: AnyObject) {
+        
+        
+        
+        var appD = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        var entity = NSEntityDescription.entityForName("Post", inManagedObjectContext: appD.managedObjectContext)
+        
+        var postObject = NSEntityDescription.insertNewObjectForEntityForName("Post", inManagedObjectContext: appD.managedObjectContext) as NSManagedObject
+        
+        postObject.setValue("jo@theironyard.com", forKey: "user")
+        postObject.setValue("this is a cool title", forKey: "title")
+        
+        appD.saveContext()
+        
+        var fetchRequest = NSFetchRequest()
+        
+        
+        
+        fetchRequest.entity = entity
+        
+        var predicate = NSPredicate(format:"user = 'jo@theironyard.com'")
+        
+        fetchRequest.predicate = predicate
+        
+        var sortDescriptor = NSSortDescriptor(key: "posted_at", ascending: true)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        var fetchedObjects = appD.managedObjectContext!.executeFetchRequest(fetchRequest, error: nil)
+        
+        println(fetchedObjects)
+
+        
+        
+        
+        let newPostURL = API + "posts?post[title]=\(titleField.text)&post[content]=\(contentField.text)"
+        
+        let encodedString = newPostURL.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        
+        var request = NSMutableURLRequest(URL: NSURL(string: encodedString!))
+        
+        request.setValue("zxcasdqwe", forHTTPHeaderField: "AUTH_TOKEN")
+        
+        request.HTTPMethod = "POST"
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue())  { (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            let info = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil)!
+            
+            println (info)
+            
+            
+
+            
+        }
+        
+    }
+    
+    @IBAction func getMyPosts(sender: AnyObject) {
+        
+        let myPostsURL = API + "posts/mine"
+        
+        var request = NSMutableURLRequest(URL: NSURL(string: myPostsURL))
+        
+        
+        
+        
+        request.setValue("zxcasdqwe", forHTTPHeaderField: "AUTH_TOKEN")
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue())  { (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            let posts = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSArray
+            
+            println (posts)
+            
+        }
+
+    }
+    
+    @IBAction func getAllPosts(sender: AnyObject) {
+        
+        let allPostsURL = API + "posts"
+        
+        let request = NSURLRequest(URL: NSURL(string: allPostsURL))
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue())  { (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            let posts = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSArray
+            
+            println (posts)
+            
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
